ee8b6c12a73ea73657aba860a4954543f0de91ec
Ricardo Miralles <ricmiber@gmail.com>
2024-05-23 13:52:02 +0200

    Add: Adding Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,42 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+
+const navIcons = [
+    {
+        src: '/assets/icons/search.svg', alt: 'search', link: '/'
+    },
+    {
+        src: '/assets/icons/heart.svg', alt: 'wishlist', link: '/wishlist'
+    },
+    {
+        src: '/assets/icons/user.svg', alt: 'profile', link: '/profile'
+    }
+]
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <header className='w-full'>
+        <nav className='nav'>
+            <Link href='/' className='flex items-center gap-1'>
+                <Image src={'/assets/icons/logo.svg'} width={40} height={40} alt='logo' />
+                <p className='nav-logo'>
+                    Price <span className='text-primary'>Tracker</span>
+                </p>
+            </Link>
+            <div className='flex items-center gap-4'>
+                {navIcons.map((icon, index) => (
+                    <Link href={icon.link} key={icon.alt}>
+                        <Image
+                            src={icon.src} 
+                            className='object-contain'
+                            width={26} 
+                            height={26} 
+                            alt={icon.alt} />
+                    </Link>
+                ))}
+            </div>
+        </nav>
+    </header>
   )
 }
